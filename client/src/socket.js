7210668d543dde4325c8302e65cf2931590c7bff
Annuuurag <anuragrajbonshi84@gmail.com.com>
2025-12-25 18:31:17 +0000

    did some changes

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,16 @@
-import { io } from "socket.io-client";
+/*import { io } from "socket.io-client";
 
 const socket = io("http://3.110.135.150:9000");
+
+export default socket;
+*/
+
+import { io } from "socket.io-client";
+
+const SOCKET_URL = import.meta.env.VITE_BACKEND_URL;
+
+const socket = io(SOCKET_URL, {
+  transports: ["websocket"],
+});
 
 export default socket;
